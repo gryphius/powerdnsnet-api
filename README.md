2b52345781bf866b341765488e79f3b262052764
Oli Schacher <oli@fuglu.org>
2015-03-08 19:53:34 +0100

    implement the rest

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-Client API implementation for powerdns.net DNS Hosting
+python client API implementation for powerdns.net DNS hosting
+
+Notes:
+ * instantiate the PowerdnsNet object with the API key found in your powerdns control panel (MYPOWERDNS -> API Access)
+ * if a method expects a 'zone' argument this can be either a Zone object as returned by list_zones/add_native_domain, a zone name or the zone id
+ * if a method expects a 'record' argument this can be either a Record object as returned by list_records/add_record_to_zone or a record id
+ * the API does not to any client-side input validation
+
+Example usage
+
+```python
+
+#!/usr/bin/python
+import os
+from powerdnsnet import PowerdnsNet
+
+apikey=open(os.path.expanduser("~/powerdnsnet-api-key.txt"),'r').read().strip()
+api=PowerdnsNet(apikey)
+zones=api.list_zones()
+
+for zone in zones:
+    print "### %s ###"""%zone.name
+    print "Zone id: %s"%api.zoneid_by_name(zone.name)
+    print "Records"
+    records=api.list_records(zone)
+    for r in records:
+        print r
+    print ""
+
+## create zone
+# api.add_native_domain('example.com')
+
+## renew (add one year)
+# api.renew_zone('example.com')
+
+## add record
+#record=api.add_record_to_zone('example.com','www.example.com','127.0.0.2')
+
+## update record
+#record.content='3.2.1.0'
+#api.update_record(record)
+
+## delete record
+#api.delete_record(record)
+
+## clear zone
+#api.delete_all_records_for_domain('example.com')
+
+## delete zone
+#api.delete_zone('example.com')
+
+```
--- powerdnsnet/__init__.py
+++ powerdnsnet/__init__.py
@@ -1,21 +1,22 @@
 import xml.etree.cElementTree as et
 import urllib2
-
+import datetime
 
 class PowerdnsNet(object):
     def __init__(self,apikey):
         self.apikey=apikey
         self.baseurl="https://www.powerdns.net/services/express.asmx"
-
         self.cache_zonename_id=None
-
         self.debug=False
 
-    def add_native_domain(self,zone_name):
-        raise Exception("not implemented")
+    def add_native_domain(self,zonename):
+        """Create domain, returns Zone object"""
+        result = self.soap_request('addNativeDomain',dict(domainName=zonename))
+        self.clear_cache() # invalidate zone map. In the future we might check for existing map and add the new zone
+        return self._node_to_zone(result)
 
     def add_record_to_zone(self,zone,name,content,rtype='A',ttl=3600,prio=0):
-        """Add record to zone, returns ID of the new record"""
+        """Add record to zone, returns new record object"""
         zoneid=self.expect_zone_id(zone)
         args={
             'zoneId':zoneid,
@@ -26,24 +27,35 @@ class PowerdnsNet(object):
             'Priority':prio,
         }
         result=self.soap_request('addRecordToZone',args)
-        for child in result:
-            if child.tag=='{http://powerdns.net/express}Id':
-                return int(child.text)
+        record=self._node_to_record(result)
+        return record
 
-    def delete_all_records_for_domain(self):
-        raise Exception("not implemented")
+    def delete_all_records_for_domain(self,zone):
+        zoneid=self.expect_zone_id(zone)
+        result=self.soap_request('deleteAllRecordsForDomain',dict(zoneId=zoneid))
 
-    def delete_record_by_id(self,recordid):
+    def delete_record(self,record):
+        """Delete a record (by id)"""
+        recordid=self._expect_record_id(record)
         result=self.soap_request('deleteRecordById',dict(recordId=recordid))
 
-    def delete_zone_by_id(self):
-        raise Exception("not implemented")
+    def delete_zone(self,zone):
+        """Delete a zone (by name or id)"""
+        zoneid=self.expect_zone_id(zone)
+        result=self.soap_request('deleteZoneById',dict(zoneId=zoneid))
 
-    def delete_zone_by_name(self):
-        raise Exception("not implemented")
-
-    def update_record(self):
-        raise Exception("not implemented")
+    def update_record(self,recordobject):
+        """Rewrite a record based on recordid"""
+        assert type(recordobject)==Record
+        args={
+            'recordId':recordobject.id,
+            'Name':recordobject.name,
+            'Type':recordobject.type.upper(),
+            'Content':recordobject.content,
+            'TimeToLive':recordobject.ttl,
+            'Priority':recordobject.prio,
+        }
+        result=self.soap_request('updateRecord',args)
 
     def list_records(self,zone,rtype=None):
         """Returns a list of all records in a zone. If rtype is specified, only returns that record type. Return value is a list of Record objects"""
@@ -56,43 +68,30 @@ class PowerdnsNet(object):
         retlist=[]
 
         for rec in recordlist:
-            record=Record()
-            record.id=rec[0].text
-            record.zoneid=rec[1].text
-            record.name=rec[2].text
-            record.type=rec[3].text
-            record.content=rec[4].text
-            record.ttl=rec[5].text
-            record.priority=rec[6].text
+            record=self._node_to_record(rec)
             retlist.append(record)
         return retlist
-
-    #def list_records_by_type(self): # included in list_records
-    #    raise Exception("not implemented")
 
     def list_zones(self,updatecache=True):
         """Returns a list of all zone names"""
         zoneids={}
         zonelist=self.soap_request("listZones")
-        for zone in zonelist:
-            zoneid=None
-            zonename=None
-            for child in zone:
-                if child.tag=='{http://powerdns.net/express}Id':
-                    zoneid=child.text
-                if child.tag=='{http://powerdns.net/express}Name':
-                    zonename=child.text
+
+        zones=[]
+        for zonenode in zonelist:
+            zoneobj=self._node_to_zone(zonenode)
+            zones.append(zoneobj)
+            zoneid=zoneobj.id
+            zonename=zoneobj.name
+
             if zoneid!=None and zonename!=None:
                 zoneids[zonename]=zoneid
-            else:
-                #should we warn here?
-                continue
         self.cache_zonename_id= zoneids
-        return sorted(zoneids.keys())
+        return zones
 
-    def renew_zone(self):
-        raise Exception("not implemented")
-
+    def renew_zone(self,zone):
+        zoneid=self.expect_zone_id(zone)
+        result=self.soap_request('renewZone',dict(zoneId=zoneid))
 
     def clear_cache(self):
         """forget everything we've cached from previous calls"""
@@ -101,8 +100,20 @@ class PowerdnsNet(object):
     def expect_zone_id(self,value):
         if type(value)==int:
             return value
-        else:
+        elif type(value) in (str,unicode):
             return self.zoneid_by_name(value)
+        elif type(value)==Zone:
+            return value.id
+        else:
+            raise ValueError("Expecting Zone object/zone name/zone id")
+
+    def _expect_record_id(self,value):
+        if type(value)==int:
+            return value
+        elif type(value)==Record:
+            return value.id
+        else:
+            raise ValueError("Expecting Record object or record id")
 
     def zoneid_by_name(self,zonename):
         if self.cache_zonename_id==None:
@@ -166,6 +177,35 @@ class PowerdnsNet(object):
     def debug_out(self,msg):
         print msg
 
+    def _node_to_record(self,node):
+        """Transform xml node into Record object"""
+        record=Record()
+        record.id=int(node[0].text)
+        record.zoneid=node[1].text
+        record.name=node[2].text
+        record.type=node[3].text
+        record.content=node[4].text
+        record.ttl=node[5].text
+        record.priority=node[6].text
+        return record
+
+    def _node_to_zone(self,node):
+        """Transform xml node into Zone object"""
+        zone=Zone()
+        zone.id=int(node[0].text)
+        zone.name=node[1].text
+        zone.master=node[2].text
+        #zone.last_check=... looks broken
+        zone.type=node[4].text
+        #notified_serial= .. always 0)
+        zone.account=node[6].text
+        zone.active=bool(int(node[7].text))
+        datestr=node[8].text
+        zone.expires=datetime.datetime.strptime(datestr,"%Y-%m-%dT%H:%M:%S")
+        zone.level=node[9].text
+        zone.validdns= (node[10].text=='true')
+        return zone
+
 class APIException(Exception):
     pass
 
@@ -191,22 +231,10 @@ class Zone(object):
     def __init__(self):
         self.id=None
         self.name=None
-        self.hostnaster=None
-        self.serial=0
-        self.flags=0
-        self.active=1
-        self.ttl=0
-        self.ownerid=0
         self.master=None
-        self.expired=None
-        self.created=None
-
-
-class BaseResponse(object):
-    def __init__(self):
-        self.code=0
-        self.description=''
-
-class AddNativeDomain_Response(BaseResponse):
-    def __init__(self):
-        pass
+        self.type=None
+        self.account=None
+        self.active=True
+        self.expires=None
+        self.level=None
+        self.validdns=None
